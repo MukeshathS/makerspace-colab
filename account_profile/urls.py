653d2bbae6279f35e6d8fa2ff016b0e8e8ec2956
MukeshnathS <mukeshnaths@users.noreply.github.com>
2020-07-15 20:23:42 +0530

    Settings changes

--- account_profile/urls.py
+++ account_profile/urls.py
@@ -13,14 +13,11 @@ app_name = "users"
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
-    path('login/', LoginView.as_view(template_name='profile/login.html', extra_content={'user': 'Hello Guest!'}), name='login'),
+    path('login/', LoginView.as_view(template_name='profile/login.html'), name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
-
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='profile/registration/password_reset_done.html'),
-     name='password_reset_done'),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='profile/registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='profile/registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='profile/registration/password_reset_form.html'), name='password_reset'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='profile/registration/password_reset_complete.html'),
-     name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='profile/registration/password_reset_complete.html'),name='password_reset_complete'),
 ]
