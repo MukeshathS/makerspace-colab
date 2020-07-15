455c3e8e936ebb6e9102ecdde8126ecfd639c831
MukeshnathS <mukeshnaths@users.noreply.github.com>
2020-07-16 00:04:53 +0530

    settings.py email setings changes.

--- ResumeColab/settings.py
+++ ResumeColab/settings.py
@@ -158,11 +158,11 @@ LOGIN_EXEMPT_URLS = {
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = '******************'
-# EMAIL_HOST_PASSWORD = '******************'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'makerspace.colab@gmail.com'
+EMAIL_HOST_PASSWORD = 'MakerspaceColab@93'
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
